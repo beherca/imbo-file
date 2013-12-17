9dc832ed71aac76771210adcee2dc1ee010712a2
Kai Li <kai.li@tranzvision.com.cn>
2013-12-17 16:34:37 +0800

    change the xml images to files

--- library/ImboFile/Http/Response/Formatter/XML.php
+++ library/ImboFile/Http/Response/Formatter/XML.php
@@ -8,7 +8,7 @@
  * distributed with this source code.
  */
 
-namespace Imbo\Http\Response\Formatter;
+namespace ImboFile\Http\Response\Formatter;
 
 use Imbo\Http\Response\Formatter\XML as ImboXML,
     ImboFile\Model;
@@ -24,75 +24,75 @@ class XML extends ImboXML {
   /**
    * {@inheritdoc}
    */
-  public function formatImages(Model\Images $model) {
-    $images = '';
+  public function formatFiles(Model\Files $model) {
+    $files = '';
   
     if ($fields = $model->getFields()) {
       $fields = array_fill_keys($fields, 1);
     }
   
-    foreach ($model->getImages() as $image) {
-      $images .= '<image>';
+    foreach ($model->getFiles() as $file) {
+      $files .= '<file>';
   
       if (empty($fields) || isset($fields['publicKey'])) {
-        $images .= '<publicKey>' . $image->getPublicKey() . '</publicKey>';
+        $files .= '<publicKey>' . $file->getPublicKey() . '</publicKey>';
       }
   
-      if (empty($fields) || isset($fields['imageIdentifier'])) {
-        $images .= '<imageIdentifier>' . $image->getImageIdentifier() . '</imageIdentifier>';
+      if (empty($fields) || isset($fields['fileIdentifier'])) {
+        $files .= '<fileIdentifier>' . $file->getFileIdentifier() . '</fileIdentifier>';
       }
   
       if (empty($fields) || isset($fields['checksum'])) {
-        $images .= '<checksum>' . $image->getChecksum() . '</checksum>';
+        $files .= '<checksum>' . $file->getChecksum() . '</checksum>';
       }
   
       if (empty($fields) || isset($fields['mime'])) {
-        $images .= '<mime>' . $image->getMimeType() . '</mime>';
+        $files .= '<mime>' . $file->getMimeType() . '</mime>';
       }
   
       if (empty($fields) || isset($fields['extension'])) {
-        $images .= '<extension>' . $image->getExtension() . '</extension>';
+        $files .= '<extension>' . $file->getExtension() . '</extension>';
       }
   
       if (empty($fields) || isset($fields['added'])) {
-        $images .= '<added>' . $this->dateFormatter->formatDate($image->getAddedDate()) . '</added>';
+        $files .= '<added>' . $this->dateFormatter->formatDate($file->getAddedDate()) . '</added>';
       }
   
       if (empty($fields) || isset($fields['updated'])) {
-        $images .= '<updated>' . $this->dateFormatter->formatDate($image->getUpdatedDate()) . '</updated>';
+        $files .= '<updated>' . $this->dateFormatter->formatDate($file->getUpdatedDate()) . '</updated>';
       }
   
       if (empty($fields) || isset($fields['size'])) {
-        $images .= '<size>' . $image->getFilesize() . '</size>';
+        $files .= '<size>' . $file->getFilesize() . '</size>';
       }
   
       if (empty($fields) || isset($fields['width'])) {
-        $images .= '<width>' . $image->getWidth() . '</width>';
+        $files .= '<width>' . $file->getWidth() . '</width>';
       }
   
       if (empty($fields) || isset($fields['height'])) {
-        $images .= '<height>' . $image->getHeight() . '</height>';
+        $files .= '<height>' . $file->getHeight() . '</height>';
       }
   
-      $metadata = $image->getMetadata();
+      $metadata = $file->getMetadata();
   
       if (is_array($metadata) && (empty($fields) || isset($fields['metadata']))) {
-        $images .= '<metadata>';
+        $files .= '<metadata>';
   
         foreach ($metadata as $key => $value) {
-          $images .= '<tag key="' . $key . '">' . $value . '</tag>';
+          $files .= '<tag key="' . $key . '">' . $value . '</tag>';
         }
   
-        $images .= '</metadata>';
+        $files .= '</metadata>';
       }
   
-      $images .= '</image>';
+      $files .= '</file>';
     }
   
     return <<<IMAGES
 <?xml version="1.0" encoding="UTF-8"?>
 <imbo>
-  <images>{$images}</images>
+  <files>{$files}</files>
 </imbo>
 IMAGES;
   }
